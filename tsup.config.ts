b635e2a9322483ebd0d81e47c8aee65a4364a836
Jaron Fort <jaronfort@gmail.com>
2025-01-20 15:04:20 -0800

    build: remove tsup config

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-import tsconfigPaths from 'tsconfig-paths';
-
-export default defineConfig({
-	entry: ['src/index.ts'], // Adjust according to your entry file
-	outDir: 'dist',
-	sourcemap: true,
-	clean: true,
-});
